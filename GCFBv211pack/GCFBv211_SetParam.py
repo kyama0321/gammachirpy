d33336213a77042873249c8472157e857f5a7b9d
kyama0321 <kyama0321@gmail.com>
2022-05-15 21:23:51 +0900

    Add: parameter settings (initial)

---
+++ GCFBv211pack/GCFBv211_SetParam.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import datetime
+import sys
+import utils
+
+
+class GCresp:
+    Fr1 = []
+    ERBspace1 = []
+    Ef = []
+    b1val = []
+    c1val = []
+    Fp1 = []
+    b2val = []
+    c1val = []
+
+class LvlEst:
+    LctERB = []
+    DecayHL = []
+    b2 = []
+    c2 = []
+    frat = []
+    RMStoSPLdB = []
+    Weight = []
+    RefdB = []
+    Pwr = []
+    ExpDecayVal = []
+    NchShift = []
+    NchLvlEst = []
+    LvlLinMinLim = []
+    LvlLinRef = []
+
+
+def SetParam(GCparam):
+    """Setting Default Parameters for GCFBv2
+
+    Args:
+        GCparam (struct): Your preset gammachirp parameters
+            .fs: Sampling rate (default: 48000)
+            .NumCh: Number of Channels (default: 100)
+            .FRange: Frequency Range of GCFB (default: [100, 6000]) specifying asymtopic freq. of passive GC (Fr1)
+
+    Returns:
+        GCparam (struct): GCparam values
+    """
+    if not hasattr(GCparam, 'fs'):
+        GCparam.fs = 48000
+
+    if not hasattr(GCparam, 'OutMidCrct'):
+        GCparam.OutMidCrct = 'ELC'
+        # if no OutMidCrct is not necessary, specify GCparam.OutMidCrct = 'no'
+
+    if not hasattr(GCparam, 'NumCh'):
+        GCparam.NumCh = 100
+
+    if not hasattr(GCparam, 'FRange'):
+        GCparam.FRange = np.array([100, 6000])
+    
+    # Gammachirp parameters
+    if not hasattr(GCparam, 'n'):
+        GCparam.n = 4 # default gammatone & gammachirp
+
+    # Convention
+    if not hasattr(GCparam, 'b1'):
+        GCparam.b1 = np.array([1.81, 0]) # b1 becomes two coeffs in v210 (18 Apr. 2015). Frequency independent by 0. 
+
+    if len(GCparam.b1) == 1:
+        GCparam.b1.append(0) # frequency independent by 0
+
+    if not hasattr(GCparam, 'c1'):
+        GCparam.c1 = np.array([-2.96, 0]) # c1 becomes two coeffs. in v210 (18 Apr. 2015). Frequency independent by 0.
+
+    if len(GCparam.c1) == 1:
+        GCparam.c1.append(0) # frequency independent by 0
+    
+    if not hasattr(GCparam, 'frat'):
+        GCparam.frat = np.array([[0.4666, 0], [0.0109, 0]])
+    
+    if not hasattr(GCparam, 'b2'):
+        GCparam.b2 = np.array([[2.17, 0], [0, 0]]) # no level-dependency  (8 Jul 05)
+
+    if not hasattr(GCparam, 'c2'):
+        GCparam.c2 = np.array([[2.20, 0], [0, 0]]) # no level-dependency; no freq-dependency (3 Jun 05)
+
+    if not hasattr(GCparam, 'Ctrl'):
+        GCparam.Ctrl = 'static'      
+
+    if not hasattr(GCparam, 'Ctrl'):
+        GCparam.Ctrl = 'static'
+    if 'fix' in GCparam.Ctrl:
+        GCparam.Ctrl = 'static'
+    if 'tim' in GCparam.Ctrl:
+        GCparam.Ctrl = 'dynamic'
+    if not 'sta' in GCparam.Ctrl and not 'dyn' in GCparam.Ctrl and not 'lev' in GCparam.Ctrl:
+        print("Specify GCparam.Ctrl:  'static', 'dynamic', or 'level(-estimation). (old version 'fixed'/'time-varying')", file=sys.stderr)
+        sys.exit(1)
+
+    if not hasattr(GCparam, 'GainCmpnstdB'):
+        GCparam.GainCmpnstdB = -1 # in dB. when LvlEst.c2==2.2, 1 July 2005
+
+    """
+    Parameters for level estimation
+    """
+    if hasattr(GCparam, 'PpgcRef') or hasattr(GCparam, 'LvlRefdB'):
+        print("The parameter 'GCparam.PpgcRef' is obsolete.")
+        print("The parameter 'GCparam.LvlRefdB' is obsolete.")
+        print("Please change it to 'GCparam.GainRefdB'", file=sys.stderr)
+        sys.exit(1)
+    
+    if not hasattr(GCparam, 'GainRefdB'):
+        GCparam.GainRefdB = 50 # reference Ppgc level for gain normalization
+
+    if not hasattr(GCparam, 'LeveldBscGCFB'):
+        GCparam.LeveldBscGCFB = GCparam.GainRefdB # use it as default
+
+    if not hasattr(GCparam, 'LvlEst'):
+        GCparam.LvlEst = LvlEst()
+
+    if len(GCparam.LvlEst.LctERB) == 0:
+        #GCparam.LvlEst.LctERB = 1.0
+        # Location of Level Estimation pGC relative to the signal pGC in ERB
+        # see testGC_LctERB.py for fitting result. 10 Sept 2004
+        GCparam.LvlEst.LctERB = 1.5;   # 16 July 05
+
+    if len(GCparam.LvlEst.DecayHL) == 0:
+        GCparam.LvlEst.DecayHL = 0.5; # 18 July 2005
+
+    if len(GCparam.LvlEst.b2) == 0:
+        GCparam.LvlEst.b2 = GCparam.b2[0, 0]
+
+    if len(GCparam.LvlEst.c2) == 0:
+        GCparam.LvlEst.c2 = GCparam.c2[0, 0]
+
+    if len(GCparam.LvlEst.frat) == 0:
+        # GCparam.LvlEst.frat = 1.1 #  when b=2.01 & c=2.20
+        GCparam.LvlEst.frat = 1.08 # peak of cGC ~= 0 dB (b2=2.17 & c2=2.20)
+
+    if len(GCparam.LvlEst.RMStoSPLdB) == 0:
+        GCparam.LvlEst.RMStoSPLdB = 30 # 1 rms == 30 dB SPL for Meddis IHC
+
+    if len(GCparam.LvlEst.Weight) == 0:
+        GCparam.LvlEst.Weight = 0.5
+
+    if len(GCparam.LvlEst.RefdB) == 0:
+        GCparam.LvlEst.RefdB = 50 # 50 dB SPL
+
+    if len(GCparam.LvlEst.Pwr) == 0:
+        GCparam.LvlEst.Pwr = np.array([1.5, 0.5]) # Weight for pGC & cGC
+
+    # new 19 Dec 2011
+    if not hasattr(GCparam, 'NumUpdateAsymCmp'):
+        # GCparam.NumUpdateAsymCmp = 3 # updte every 3 samples (== 3*GCFBv207)
+        GCparam.NumUpdateAsymCmp = 1 # samply-by-sample (==GCFBv207)
+
+    """
+    GCresp
+    """
+    Fr1, ERBrate1 = utils.EqualFreqScale('ERB', GCparam.NumCh, GCparam.FRange)
+    GCresp.Fr1 = np.array([Fr1]).T
+    GCresp.ERBspace1 = np.mean(np.diff(ERBrate1))
+    ERBrate, ERBw = utils.Freq2ERB(GCresp.Fr1)
+    ERBrate1kHz, ERBw1kHz = utils.Freq2ERB(1000)
+    GCresp.Ef = ERBrate/ERBrate1kHz - 1
+
+    OneVec = np.ones([GCparam.NumCh, 1])
+    GCresp.b1val = GCparam.b1[0]*OneVec + GCparam.b1[1]*GCresp.Ef
+    GCresp.c1val = GCparam.c1[0]*OneVec + GCparam.c1[1]*GCresp.Ef
+
+    GCresp.Fp1, _ = utils.Fr2Fpeak(GCparam.n, GCresp.b1val, GCresp.c1val, GCresp.Fr1)
+
+    GCresp.b2val = GCparam.b2[0, 0]*OneVec + GCparam.b2[0, 1]*GCresp.Ef
+    GCresp.c2val = GCparam.c2[0, 0]*OneVec + GCparam.c2[0, 1]*GCresp.Ef
+    
+    """
+    Set Params estimation circuit
+    """
+    # keep LvlEst params  3 Dec 2013
+    ExpDecayVal = np.exp(-1/(GCparam.LvlEst.DecayHL*GCparam.fs/1000)*np.log(2)) # decay exp
+    NchShift = np.round(GCparam.LvlEst.LctERB/GCresp.ERBspace1)
+    NchLvlEst = np.minimum(np.maximum(1, np.array([np.arange(GCparam.NumCh)+1]).T+NchShift), GCparam.NumCh) # shift in NumCh [1:NumCh]
+    LvlLinMinLim = 10**(-GCparam.LvlEst.RMStoSPLdB/20) # minimum sould be SPL 0 dB
+    LvlLinRef = 10**((GCparam.LvlEst.RefdB - GCparam.LvlEst.RMStoSPLdB)/20)
+
+    GCparam.LvlEst.ExpDecayVal = ExpDecayVal
+    GCparam.LvlEst.ERBspace1 = GCresp.ERBspace1
+    GCparam.LvlEst.NchShift = NchShift
+    GCparam.LvlEst.NchLvlEst = NchLvlEst
+    GCparam.LvlEst.LvlLinMinLim = LvlLinMinLim
+    GCparam.LvlEst.LvlLinRef = LvlLinRef
+
+    return GCparam, GCresp
--- GCFBv211pack/utils.py
+++ GCFBv211pack/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 import numpy as np
 import wave as wave
 
@@ -25,6 +26,7 @@ def audioread(filepath):
     wav.close()
     wavSnd = wavSnd/abs(np.iinfo(np.int16).min) # float (-1 ~ +1)
     return wavSnd, wavFs
+
 
 def rms(x):
     """
@@ -62,9 +64,7 @@ def Eqlz2MeddisHCLevel(Snd, OutLeveldB, *args):
 
     Reference:
         Meddis (1986), JASA, 79(3),pp.702-711.
-
     """
-
     SourceLevel = np.sqrt(np.mean(Snd**2)) * 10**(30/20) # level in terms of Meddis Level
 
     Amp = (10**(OutLeveldB/20))/SourceLevel
@@ -73,3 +73,147 @@ def Eqlz2MeddisHCLevel(Snd, OutLeveldB, *args):
     AmpdB = [OutLeveldB, 20*np.log10(Amp), 20*np.log10(SourceLevel)]
 
     return SndEqM, AmpdB
+
+
+def EqualFreqScale(NameScale, NumCh, RangeFreq):
+    """Calculation of Equal Frequency scale on ERB/Mel/Log/Linear scale
+
+    Args:
+        NameScale (string): 'ERB', 'mel', 'log', 'linear'
+        NumCh (int): Number of channels
+        RangeFreq (array): Frequency Range
+
+    Returns:
+        Frs (array): Fr vector
+        WFval (array): Wraped freq. value
+    """
+    eps = np.finfo(float).eps # epsilon
+
+    if NameScale == 'linear':
+        RangeWF = RangeFreq
+        dWF = np.diff(RangeWF) / (NumCh-1)
+        WFvals = np.linspace(RangeWF[0], RangeWF[1]+eps*1000, dWF)
+        Frs = WFvals
+
+    elif NameScale == 'mel':
+        RangeWF = Freq2FMel(RangeFreq)
+        dWF = np.diff(RangeWF) / (NumCh-1)
+        WFvals = np.linspace(RangeWF[0], RangeWF[1]+eps*1000, dWF)
+        Frs = Mel2Freq(WFvals)
+
+    elif NameScale == 'ERB':
+        RangeWF, _ = Freq2ERB(RangeFreq)
+        dWF = np.diff(RangeWF) / (NumCh-1)
+        WFvals = np.arange(RangeWF[0], RangeWF[1]+eps*1000, dWF)
+        Frs, _ = ERB2Freq(WFvals)
+
+    elif NameScale == 'log':
+        if min(RangeFreq) < 50:
+            print("min(RangeFreq) < 50. Rplaced by 50.")
+            RangeFreq[0] = 50
+        RangeWF = np.log10(RangeFreq)
+        dWF = np.diff(RangeWF) / (NumCh-1)
+        WFvals = np.linspace(RangeWF[0], RangeWF[1]+eps*1000, dWF)
+        Frs = 10**(WFvals)
+    else:
+        help(EqualFreqScale)
+        print("Specify NameScale correctly", file=sys.stderr)
+        sys.exit(1)
+    
+    return Frs, WFvals
+
+
+def Freq2FMel(freq):
+    """Convert mel to linear frequency
+
+    Args:
+        freq (array): linaer-scale frequency [Hz] 
+
+    Returns:
+        mel (array): mel-scale frequency [mel]
+
+    Note:
+        The function was made by the GammachirPy project because there is not original code of "mel2freq" in GCFBv211pack 
+    """
+    mel = 2595 * np.log10(1+freq/700)
+    return mel
+
+
+def Mel2Freq(mel):
+    """Convert mel to linear frequency
+
+    Args:
+        mel (array): mel-scale frequency [mel] 
+
+    Returns:
+        freq (array): linear-scale frequency [Hz]
+
+    Note:
+        The function was made by the GammachirPy project because there is not original code of "mel2freq" in GCFBv211pack 
+    """
+    freq = 700 * ((10**(mel/2595))-1)
+    return freq
+
+
+def Freq2ERB(cf):
+    """Convert linear frequency to ERB
+
+    Args:
+        cf (array): center frequency in linaer-scale [Hz] 
+
+    Returns:
+        ERBrate (array): ERB_N rate [ERB_N] or [cam] 
+        ERBwidth (array): ERB_N Bandwidth [Hz]
+    """
+    # Warnig for frequency range
+    cfmin = 50
+    cfmax = 12000
+    if np.min(cf) < cfmin or np.max(cf) > cfmax:
+        print("Warning : Min or max frequency exceeds the proper ERB range: "
+            +"{} (Hz) <= Fc <= {} (Hz)".format(cfmin, cfmax), file=sys.stderr)
+        sys.exit(1)
+
+    ERBrate = 21.4 * np.log10(4.37*cf/1000+1)
+    ERBwidth = 24.7 * (4.37*cf/1000+1)
+
+    return ERBrate, ERBwidth
+
+
+def ERB2Freq(ERBrate):
+    """Convert ERBrate to linear frequency
+
+    Args:
+        ERBrate (array): ERB_N rate [ERB_N] or [cam] 
+    
+    Returns:
+        cf (array): center frequency in linaer-scale [Hz] 
+        ERBwidth (array): ERB_N Bandwidth [Hz]
+    """
+    cf = (10**(ERBrate/21.4)-1) / 4.37 * 1000
+    ERBwidth = 24.7 * (4.37*cf/1000+1)
+
+    return cf, ERBwidth
+
+
+def Fr2Fpeak(n, b, c, fr):
+    """Estimate fpeak from fr
+
+    Args:
+        n (float): a parameter of the gammachirp
+        b (float): a parameter of the gammachirp
+        c (float): a parameter of the gammachirp
+        fr (float): fr
+
+    Returns:
+        fpeak (float): peak frequency
+        ERBw (float): ERBwidth at fr
+    """
+    #n = np.array([n]).T
+    #b = np.array(b).T
+    #c = np.array(c).T
+    #fr = np.array(fr).T
+
+    _, ERBw = Freq2ERB(fr)
+    fpeak = fr + c*ERBw*b/n
+
+    return fpeak, ERBw

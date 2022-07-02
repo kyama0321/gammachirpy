a4a509c1641857357e6936a4e2569f62f3f47eab
kyama0321 <kyama0321@gmail.com>
2022-07-02 20:08:59 +0900

    Mod: changed name and f-string

--- GCFBv211pack/testGCFBv211.py
+++ GCFBv211pack/testGCFBv211.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import utils
-import GCFBv211 as gcfb_Main
+import GCFBv211 as gcfb_main
 
 
-class GCparam_default:
+class GCparamDefault:
     fs = 48000
     NumCh = 100
     FRange = np.array([100, 6000])
@@ -16,55 +16,49 @@ class GCparam_default:
     # Ctrl = "static" # or 'fixed'
 
 
-# Stiuli : a simple pulse train
-fs = 48000
-Tp = 10 # (ms) 100 Hz pulse train
-Snd = np.array(([1]+[0]*int(Tp*fs/1000-1))*10)
-Tsnd = len(Snd)/fs
-print("Duration of sound = {} (ms)".format(Tsnd*1000))
+def main():
+    # Stiuli : a simple pulse train
+    fs = 48000
+    t_pulse = 10 # (ms) 100 Hz pulse train
+    snd = np.array(([1]+[0]*int(t_pulse*fs/1000-1))*10)
+    t_snd = len(snd)/fs
+    print("Duration of sound = {} (ms)".format(t_snd*1000))
 
-SigSPLlist = np.arange(40, 100, 20)
-cnt = 0
+    list_dbspl = np.arange(40, 100, 20)
+    cnt = 0
 
-for SwDySt in range(2): # 1: only dynamic, 2: dynamic and static
+    for sw_ctrl in range(2): # 1: only dynamic, 2: dynamic and static
 
-    fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-    for SwSPL in range(len(SigSPLlist)):
-        SigSPL = SigSPLlist[SwSPL]
-        Snd, _ = utils.Eqlz2MeddisHCLevel(Snd, SigSPL)
+        for sw_dbspl in range(len(list_dbspl)):
+            dbspl = list_dbspl[sw_dbspl]
+            snd_eq, _ = utils.Eqlz2MeddisHCLevel(snd, dbspl)
 
-        # GCFB
-        GCparam = GCparam_default() # reset all
-        if SwDySt == 0: 
-            GCparam.Ctrl = "dynamic"
-            # GCparam.Ctrl = "static" # for checking
-        else: 
-            GCparam.Ctrl = "static"
-        
-        Tstart = time.time()
-        cGCout, pGCout, GCparam, GCrest = gcfb_Main.GCFBv211(Snd, GCparam)
+            # Set paramteres for dcGC
+            GCparam = GCparamDefault() # reset all
+            if sw_ctrl == 0: 
+                GCparam.Ctrl = "dynamic"
+            else: 
+                GCparam.Ctrl = "static"
+            
+            # dcGC processing
+            t_start = time.time()
+            cGCout, pGCout, GCparam, GCrest = gcfb_main.GCFBv211(snd_eq, GCparam)
+            t_end = time.time()
+            print(f"Elapsed time is {np.round(t_end-t_start, 4)} (sec) = \
+                    {np.round((t_end-t_start)/t_snd, 4)} times RealTime.")
+            
+            # Plot
+            ax = plt.subplot(len(list_dbspl), 1, sw_dbspl+1)
+            plt.imshow(np.maximum(cGCout, 0), aspect='auto', origin='lower', cmap='jet')
+            ax.set_title(f"GCFB control = {GCparam.Ctrl}; Signal Level = {dbspl} dB SPL")
+            ax.set_yticks([0, 20, 40, 60, 80, 100])
+            plt.tight_layout()
+            plt.pause(0.05)
 
-        Tend = time.time()
-        print("Elapsed time is {} (sec) = {} times RealTime."\
-            .format(np.round(Tend-Tstart, 4), np.round((Tend-Tstart)/Tsnd, 4)))
+    plt.show()
 
-        ax = plt.subplot(len(SigSPLlist), 1, SwSPL+1)
-        plt.imshow(np.maximum(cGCout, 0), aspect='auto', origin='lower', cmap='jet')
-        ax.set_title("GCFB control = {}; Signal Level = {} dB SPL"\
-            .format(GCparam.Ctrl, SigSPL))
-        ax.set_yticks([0, 20, 40, 60, 80, 100])
-        plt.tight_layout()
-        plt.pause(0.05)
 
-plt.show()
-
-"""
-x = np.array(range(len(Snd)))/fs
-fig, ax = plt.subplots(figsize=(8,4))
-plt.plot(x, Snd)
-ax.set_xlabel("Time [sec]")
-ax.set_ylabel("Amplitude")
-plt.ylim(-1.05, 1.05)
-plt.show()
-"""
+if __name__ == '__main__':
+    main()

504b998db84b736e8ccef415cbdfe300a6dc1e84
kyama0321 <kyama0321@gmail.com>
2022-07-02 23:40:43 +0900

    Mod: filename and variables

--- GCFBv211pack/GammaChirp.py
+++ GCFBv211pack/GammaChirp.py
@@ -1,82 +1,80 @@
 # -*- coding: utf-8 -*-
-import sys
 import numpy as np
 from scipy import signal
 import utils
-import matplotlib.pyplot as plt
 
 
-def gammachirp(Frs, SR, OrderG=4, CoefERBw=1.019, CoefC=0, Phase=0, SwCarr='cos', SwNorm='no'):
+def gammachirp(frs, sr, order_g=4, coef_erbw=1.019, coef_c=0, phase=0, sw_carr='cos', sw_norm='no'):
     """Gammachirp : Theoretical auditory filter
-    gc(t) = t^(n-1) exp(-2 pi b ERB(Frs)) cos(2*pi*Frs*t + c ln t + phase)
+    gc(t) = t^(n-1) exp(-2 pi b ERB(frs)) cos(2*pi*frs*t + c ln t + phase)
 
     Args:
-        Frs (array_like): Asymptotic Frequency
-        SR (int): Sampling frequency
-        OrderG (int, optional): Order of Gamma function t^(OrderG-1), n. Defaults to 4.
-        CoefERBw (float, optional): Coeficient -> exp(-2*pi*CoefERBw*ERB(f)), b. Defaults to 1.019.
-        CoefC (int, optional): Coeficient -> exp(j*2*pi*Frs + CoefC*ln(t)), c. Defaults to 0.
-        Phase (int, optional): Start Phase(0 ~ 2*pi). Defaults to 0.
-        SwCarr (str, optional): Carrier ('cos','sin','complex','envelope' with 3 letters). Defaults to 'cos'.
-        SwNorm (str, optional): Normalization of peak spectrum level ('no', 'peak'). Defaults to 'no'.
+        frs (array_like): Asymptotic Frequency
+        sr (int): Sampling frequency
+        order_g (int, optional): Order of Gamma function t^(order_g-1), n. Defaults to 4.
+        coef_erbw (float, optional): Coeficient -> exp(-2*pi*coef_erbw*ERB(f)), b. Defaults to 1.019.
+        coef_c (int, optional): Coeficient -> exp(j*2*pi*frs + coef_c*ln(t)), c. Defaults to 0.
+        phase (int, optional): Start phase(0 ~ 2*pi). Defaults to 0.
+        sw_carr (str, optional): Carrier ('cos','sin','complex','envelope' with 3 letters). Defaults to 'cos'.
+        sw_norm (str, optional): Normalization of peak spectrum level ('no', 'peak'). Defaults to 'no'.
 
     Returns:
-        GC (array_like): GammaChirp
-        LenGC (array_like): Length of GC for each channel
-        Fps (array_like): Peak frequency
-        InstFreq (array_like): Instanteneous frequency
+        gc (array_like): GammaChirp
+        len_gc (array_like): Length of gc for each channel
+        fps (array_like): Peak frequency
+        inst_freq (array_like): Instanteneous frequency
     """    
 
-    if len(SwCarr) == 0:
-        SwCarr = 'cos'
+    if len(sw_carr) == 0:
+        sw_carr = 'cos'
 
-    NumCh = len(Frs)
-    OrderG = OrderG * np.ones((NumCh, 1))
-    CoefERBw = CoefERBw * np.ones((NumCh, 1))
-    CoefC = CoefC * np.ones((NumCh, 1))
-    Phase = Phase * np.ones((NumCh, 1))
+    num_ch = len(frs)
+    order_g = order_g * np.ones((num_ch, 1))
+    coef_erbw = coef_erbw * np.ones((num_ch, 1))
+    coef_c = coef_c * np.ones((num_ch, 1))
+    phase = phase * np.ones((num_ch, 1))
 
-    ERBrate, ERBw = utils.Freq2ERB(Frs)
-    LenGC1kHz = (40*max(OrderG)/max(CoefERBw) + 200) * SR/16000
-    _, ERBw1kHz = utils.Freq2ERB(1000)
+    _, erbw = utils.Freq2ERB(frs)
+    len_gc_1khz = (40*max(order_g)/max(coef_erbw) + 200) * sr/16000
+    _, erbw_1khz = utils.Freq2ERB(1000)
 
-    if SwCarr == 'sin':
-        Phase = Phase - np.pi/2*np.ones((1,NumCh))
+    if sw_carr == 'sin':
+        phase = phase - np.pi/2*np.ones((1,num_ch))
 
     # Phase compensation
-    Phase = Phase + CoefC * np.log(Frs/1000) # relative phase to 1kHz
+    phase = phase + coef_c * np.log(frs/1000) # relative phase to 1kHz
 
-    LenGC = np.fix(LenGC1kHz*ERBw1kHz/ERBw)
+    len_gc = np.fix(len_gc_1khz*erbw_1khz/erbw)
 
     """
     Production of GammaChirp
     """
-    GC = np.zeros((NumCh, int(max(LenGC))))
+    gc = np.zeros((num_ch, int(max(len_gc))))
 
-    Fps = utils.Fr2Fpeak(OrderG, CoefERBw, CoefC, Frs) # Peak freq.
-    InstFreq = np.zeros((NumCh, int(max(LenGC))))
+    fps = utils.Fr2Fpeak(order_g, coef_erbw, coef_c, frs) # Peak freq.
+    inst_freq = np.zeros((num_ch, int(max(len_gc))))
 
-    for nch in range(NumCh):
-        t = np.arange(1, int(LenGC[nch]))/SR
+    for nch in range(num_ch):
+        t = np.arange(1, int(len_gc[nch]))/sr
 
-        GammaEnv = t**(OrderG[nch]-1) * np.exp(-2*np.pi*CoefERBw[nch]*ERBw[nch]*t)
-        GammaEnv = np.array([0] + list(GammaEnv/max(GammaEnv)))
+        gamma_env = t**(order_g[nch]-1) * np.exp(-2*np.pi*coef_erbw[nch]*erbw[nch]*t)
+        gamma_env = np.array([0] + list(gamma_env/max(gamma_env)))
 
-        if SwCarr == 'env': # envelope
-            Carrier = np.ones(np.shape(GammaEnv))
-        elif SwCarr == 'com': # complex
-            Carrier = np.array([0] + list(np.exp(1j*(2*np.pi*Frs[nch]*t + CoefC[nch]*np.log(t) + Phase[nch]))))
+        if sw_carr == 'env': # envelope
+            carrier = np.ones(np.shape(gamma_env))
+        elif sw_carr == 'com': # complex
+            carrier = np.array([0] + list(np.exp(1j*(2*np.pi*frs[nch]*t + coef_c[nch]*np.log(t) + phase[nch]))))
         else:
-            Carrier = np.array([0] + list(np.cos(2*np.pi*Frs[nch]*t + CoefC[nch]*np.log(t) + Phase[nch])))
+            carrier = np.array([0] + list(np.cos(2*np.pi*frs[nch]*t + coef_c[nch]*np.log(t) + phase[nch])))
         
-        GC[nch, 0: int(LenGC[nch])] = GammaEnv * Carrier
+        gc[nch, 0: int(len_gc[nch])] = gamma_env * carrier
 
-        InstFreq[nch, 0: int(LenGC[nch])] = np.array([0] + list(Frs[nch] + list(CoefC[nch]/(2*np.pi*t))))
+        inst_freq[nch, 0: int(len_gc[nch])] = np.array([0] + list(frs[nch] + list(coef_c[nch]/(2*np.pi*t))))
 
-        if SwNorm == 'peak': # peak gain normalization
-            freq, frsp = signal.freqz(GC[nch, 0: int(LenGC[nch])], 1, 2**utils.nextpow2(int(LenGC[nch])), fs=SR)
-            fp, _ = utils.Fr2Fpeak(OrderG[nch], CoefERBw[nch], CoefC[nch], Frs[nch])
+        if sw_norm == 'peak': # peak gain normalization
+            freq, frsp = signal.freqz(gc[nch, 0: int(len_gc[nch])], 1, 2**utils.nextpow2(int(len_gc[nch])), fs=sr)
+            fp, _ = utils.Fr2Fpeak(order_g[nch], coef_erbw[nch], coef_c[nch], frs[nch])
             npeak = np.argmin(np.abs(freq - fp))
-            GC[nch, :] = GC[nch, :] / np.abs(frsp[npeak])
+            gc[nch, :] = gc[nch, :] / np.abs(frsp[npeak])
 
-    return GC, LenGC, Fps, InstFreq
+    return gc, len_gc, fps, inst_freq

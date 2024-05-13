146ad4f6a7de98bea4feaa8cf44455c66d778ae2
kyama0321 <kyama0321@gmail.com>
2024-05-13 12:57:57 +0900

    Mod: changed pause time

--- gcfb_v211/test_gcfb_v211_pulse.py
+++ gcfb_v211/test_gcfb_v211_pulse.py
@@ -63,11 +63,11 @@ def main():
             ax.set_yticks([0, 20, 40, 60, 80, 100])
             ax.set_ylabel("channel")
             plt.tight_layout()
-            plt.pause(0.05)
+            plt.pause(0.5)
 
         ax.set_xlabel("time (s)")
         plt.tight_layout()
-        plt.pause(0.05)
+        plt.pause(0.5)
     
     plt.show()
 
